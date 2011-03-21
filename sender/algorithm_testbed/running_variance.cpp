1acef53341206bf2e144960f36977b027667b781
Ryan Orendorff <ryan@rdodesigns.com>
2011-03-21 07:10:45 -0400

    Updated variance test with speed.

--- sender/algorithm_testbed/running_variance.cpp
+++ sender/algorithm_testbed/running_variance.cpp
@@ -1,14 +1,49 @@
 /**
  * @file
  * @author Ryan Orendorff <ryan@rdodesigns.com>
- * @version 77 [algorithms] (Mon Mar 21 05:50:14 EDT 2011)
- * @parent dfe253bbd15c99b209f9e824900e988faf082d8a
+ * @version 78 [algorithms] (Mon Mar 21 07:10:45 EDT 2011)
+ * @parent 5fb8ec37cac7fbd385497d0d7a9d92d223b052fe
  *
  * @section DESCRIPTION
  *
- * The functions for acquiring useful data out of the Kinect using the OpenNI
- * framework. These mostly act as bridges to preexisting methods that provide
- * additional information in less convenient structs.
+ * Tests the moving window version of the variance calculation, and tests the
+ * abiility of the algorithm to correctly calculate the variance against a well
+ * behaved standard.
+ *
+ * This is done because for large values that have little difference the
+ * calculated variance can be significantly off due to the two squared factors
+ * in the running algorithm versus one squared term in the standard. Coupled
+ * with the precision limits of number representation on computers we have a
+ * catastrophic failure. However, the standard algorithm is O(n^2), while the
+ * running algorithm is O(1).
+ *
+ * Over the ranges in use by the problems in the Esoma system (which are due to
+ * real life physical constrants of space and rotation) the running algorithm
+ * performs with insignificant error (far below the measurement bounds).
+ *
+ * Sample Output under probable sizes and limits of physical parameters.
+ * Done loop of 256 elements with holding size 32 with range 20000.
+ * Average error    = 1.0434e-07.
+ * Average variance = 3.5057e+07.
+ * Percent Error    = 2.9976e-15.
+ * Time standard    = 0.000204 seconds.
+ * Time running     = 3.9e-05 seconds.
+ * Time difference  = 0.000165 seconds.
+ * Time percent     = 523.08% faster.
+ *
+ * Sample output at ridiculous scales (9 minutes of runtime run and saved).
+ * Done loop of 16384 elements with holding size 16384 with range 20000.
+ * Average error    = 7.0302e-07.
+ * Average variance = 3.2896e+07.
+ * Percent Error    = 2.1427e-14.
+ * Time standard    = 5.1213 seconds.
+ * Time running     = 0.002355 seconds.
+ * Time difference  = 5.1189 seconds.
+ * Time percent     = 2.1737e+05% faster.
+ *
+ *
+ * More information on the standard here:
+ * http://www.johndcook.com/standard_deviation.html
  *
  * This is licensed under GPLv3.
  *
@@ -43,11 +78,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PI 3.14159265
-
 using namespace std;
 
+#define PI 3.14159265
 #define SIZE 32
+#define ITER 256
+#define LIMIT 20000
+
+//#define verbose
 
 class StandardCalculations
 {
@@ -133,15 +171,23 @@ int main()
   RunningWindowCalculations running;
 
   double error_sum = 0;
+  double var_sum   = 0;
   cout.precision(5);
 
-  cout << "| Number   | Normal  | Running | Difference  |" << endl;
-  cout << "+--------------------------------------------+" << endl;
+  //---------------------------------------------------------------------------
+  // Variance Error Tests
+  // --------------------------------------------------------------------------
+#ifdef verbose
+  cout << "+--------------------------------------------------------+" << endl;
+  cout << "| Number       | Normal      | Running     | Difference  |" << endl;
+  cout << "+--------------------------------------------------------+" << endl;
+#endif
 
   // For loop to check both calculation methods.
-  for (int i = 0; i < 256; i++)
+  for (int i = 0; i < ITER; i++)
   {
-    double value = ( (rand() % 1000000) / 1000000.0 ) * 2*PI;
+    // Range is an approximate from OpenNI output.
+    double value = ( (rand() % 100000000) / 100000000.0 ) * LIMIT ;
 
     standard.addValue(value);
     running.addValue(value);
@@ -151,24 +197,79 @@ int main()
 
     double diff = var_standard - var_running;
 
+#ifdef verbose
     cout << "| ";
-    cout.width(8);
+    cout.width(12);
     cout << value;
     cout << " |";
-    cout.width(8);
+    cout.width(12);
     cout << var_standard;
     cout << " |";
-    cout.width(8);
+    cout.width(12);
     cout << var_running;
     cout << " |" ;
     cout.width(12);
     cout << diff;
     cout << " |" << endl;
+#endif
 
     error_sum += ( (diff >= 0 ? diff : -1*diff));
+      var_sum += var_standard;
   }
+#ifdef verbose
+  cout << "+--------------------------------------------------------+" << endl;
+#endif
 
-  cout << "+--------------------------------------------+" << endl;
-  cout << "Done loop, absolute error sum over 256 elements = " << error_sum << "." << endl;
+  double var_average   = var_sum / ITER;
+  double error_average = error_sum / ITER;
+  double percent = 1 - (var_average - error_average)/var_average;
+
+  cout << "Done loop of " << ITER << " elements with holding size "
+                                  << SIZE << " with range "
+                                                       << LIMIT << "." << endl;
+  cout << "Average error    = " << error_average << "." << endl;
+  cout << "Average variance = " << var_average << "." << endl;
+  cout << "Percent Error    = " << percent << "." << endl;
+
+
+  //---------------------------------------------------------------------------
+  // Clock Tests
+  // --------------------------------------------------------------------------
+  StandardCalculations speed_std;
+  RunningWindowCalculations speed_run;
+
+  clock_t start, end;
+
+  // Standard
+  start = clock();
+  for (int i = 0; i < ITER; i++) {
+    double value = ( (rand() % 100000000) / 100000000.0 ) * LIMIT ;
+    speed_std.addValue(value);
+    double toss = speed_std.variance();
+  }
+  end = clock();
+
+  double time_std = (double)(end-start)/CLOCKS_PER_SEC;
+  cout << "Time standard    = "
+  << time_std
+  << " seconds." << "\n";
+
+
+  // Running
+  start = clock();
+  for (int i = 0; i < ITER; i++) {
+    double value = ( (rand() % 100000000) / 100000000.0 ) * LIMIT ;
+    speed_run.addValue(value);
+    double toss = speed_run.variance();
+  }
+  end = clock();
+
+  double time_run = (double)(end-start)/CLOCKS_PER_SEC;
+  cout << "Time running     = "
+  << time_run
+  << " seconds." << "\n";
+
+  cout << "Time difference  = " << time_std - time_run << " seconds." << endl;
+  cout << "Time percent     = " << time_std/time_run * 100<< "% faster." << endl;
 
 }
